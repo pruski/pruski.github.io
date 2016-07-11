8bf59e39b52b903af02c4c2fca4de95e6dcb1736
pruski <konrad.pruski@gmail.com>
2016-07-12 00:39:29 +0200

    removing unused config from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -20,14 +20,4 @@ module.exports = {
         ]
     },
     devtool: '#inline-source-map'
-    // module: {
-    //     loaders: [{
-    //         test: /\.js$/,
-    //         exclude: /node_modules/,
-    //         loader: 'babel',
-    //         query: {
-    //             presets: ['es2015', 'react']
-    //         }
-    //     }]
-    // }
 };
