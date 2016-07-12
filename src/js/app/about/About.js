7c0f5580ac7484abd383f1b591a44a1e01e098ed
pruski <konrad.pruski@gmail.com>
2016-07-12 03:33:02 +0200

    adding google drive embeded cv

--- src/js/app/about/About.js
+++ src/js/app/about/About.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-class About extends React.Component {
-    render() {
-        return <h1>About me</h1>
-    }
-}
-
-export default About;
+export default () => (
+    <iframe className="cv" frameBorder="0" src="https://docs.google.com/viewer?url=https://docs.google.com/document/d/1wLXXddjAlA3LuldTJQse2pA9SinlXql-ewhuCHJE7dA/export?format%3Dpdf&embedded=true"></iframe>
+);
