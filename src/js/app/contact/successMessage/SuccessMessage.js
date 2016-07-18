27c3218ae000e248b963e122ada2da83a3c4a5ce
pruski <konrad.pruski@gmail.com>
2016-07-18 15:46:18 +0200

    extracting general .message style

--- src/js/app/contact/successMessage/SuccessMessage.js
+++ src/js/app/contact/successMessage/SuccessMessage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default () => (
-    <div className="message-sent">
+    <div className="message">
         <h3>Thank you!</h3>
         <h4>Your message has been successfully sent. I will contact you as soon as possible.</h4>
     </div>
