b4b2818ca132c53dbd7e8eaa6da5ef5092e17a74
pruski <konrad.pruski@gmail.com>
2016-07-18 06:38:40 +0200

    adding basename & base href

--- src/js/infrastructure/Root.js
+++ src/js/infrastructure/Root.js
@@ -1,14 +1,20 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
-import { Router, Route, Redirect, browserHistory } from 'react-router';
+import { Router, Route, Redirect } from 'react-router';
+import { useRouterHistory } from 'react-router'
+import { createHistory } from 'history'
 import App from 'app/App';
 import About from 'app/about';
 import Code from 'app/code';
 import Contact from 'app/contact';
 
+const history = useRouterHistory(createHistory)({
+    basename: '/'
+});
+
 const Root = ({ store }) => (
     <Provider store={store}>
-        <Router history={browserHistory}>
+        <Router history={history}>
             <Route component={App}>
                 <Redirect from="/" to="/about" />
                 <Route name="about" path="/about" component={About} />
