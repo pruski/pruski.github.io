f80f3d5a239f71acc244ccc25d5002d6c9821eb5
Konrad Pruski <konrad.pruski@gmail.com>
2016-07-25 00:32:27 +0200

    adding RWD tweaks

--- src/js/app/code/repos/repoList/RepoList.js
+++ src/js/app/code/repos/repoList/RepoList.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import {Link, withRouter} from 'react-router';
 
-export default withRouter(({repos, router: {isActive}}) => (
-    <ul className="repo-list">
-        {repos.map(repo => (
-            <li key={repo.name} className={isActive('code/' + repo.id) ? 'active' : ''}>
-                <span className={'octicon ' + (repo.parent ? 'octicon-repo-forked' : 'octicon-repo')}></span>
+export default withRouter(({repos, router: {isActive, push}}) => {
+    let options = [];
 
-                <Link to={'/code/' + repo.id}>
-                    {repo.name}
-                </Link>
-            </li>
-        ))}
-    </ul>
-));
+    return (
+        <div>
+            <ul className="repo-list">
+                {repos.map(repo => {
+                    options.push(<option value={repo.id} selected={isActive('code/' + repo.id)}>{repo.name}</option>);
+
+                    return (
+                        <li key={repo.name} className={isActive('code/' + repo.id) ? 'active' : ''}>
+                            <span className={'octicon ' + (repo.parent ? 'octicon-repo-forked' : 'octicon-repo')}></span>
+
+                            <Link to={'/code/' + repo.id}>
+                                {repo.name}
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ul>
+
+            <div className="repo-selector">
+                <select onChange={(e) => push('code/' + e.target.value)} className="form-control">
+                    <option value="">Please select a project</option>
+                    {options}
+                </select>
+            </div>
+        </div>
+    )
+});
